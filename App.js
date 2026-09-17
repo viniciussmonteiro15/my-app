c211f686b916d84a10a66a3f3bd20963d87314b5
viniciusmonteiro15 <vinicius.s.monteiro@aluno.senai.br>
2026-09-17 13:34:43 -0300

    ...

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import HomeScreen from './src/screens/HomeScreen';
-
+import { HomeScreen } from './src/screens/HomeScreen';
+ 
 export default function App() {
   return (
     <>
